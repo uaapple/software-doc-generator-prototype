86bd8a5235447bf1bcfc68e7bb68a8294d34548a
uaapple <499244437@qq.com>
2026-04-01 22:41:09 +0800

    Add skill loader

---
+++ src/services/skill-loader.js
@@ -0,0 +1,22 @@
+import { promises as fs } from "node:fs";
+import path from "node:path";
+import { config } from "../config.js";
+
+export class SkillLoader {
+  async loadSkill(fileName) {
+    return fs.readFile(path.join(config.skillDir, fileName), "utf8");
+  }
+
+  async loadAll() {
+    const files = [
+      "requirement_extraction.md",
+      "requirement_writing.md",
+      "requirement_validation.md",
+      path.join("examples", "good_examples.md"),
+      path.join("examples", "bad_examples.md")
+    ];
+
+    const pairs = await Promise.all(files.map(async (file) => [file, await this.loadSkill(file)]));
+    return Object.fromEntries(pairs);
+  }
+}
