87665bb34c030d22feacc4c5ede07e3455f17f11
uaapple <499244437@qq.com>
2026-04-01 22:41:16 +0800

    Add template service

---
+++ src/services/template-service.js
@@ -0,0 +1,9 @@
+import { readFile } from "node:fs/promises";
+import { config } from "../config.js";
+
+export class TemplateService {
+  async getTemplate() {
+    const content = await readFile(config.templatePath, "utf8");
+    return JSON.parse(content.replace(/^\uFEFF/, ""));
+  }
+}
